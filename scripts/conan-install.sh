f6b7c3d613413b5649697ad0baace35982eed484
Jonas <jonas.nussdorfer@gmail.com>
2023-03-19 06:57:48 +0000

    build profile needs c++11

--- scripts/conan-install.sh
+++ scripts/conan-install.sh
@@ -13,5 +13,5 @@ export CONAN_USER_HOME=${WORKSPACE_PATH}
 conan install ${WORKSPACE_PATH} \
     -if ${WORKSPACE_PATH}/conan-find/gtest \
     -s:h build_type=Debug \
-    -s:h compiler.libcxx=libstdc++11 \
+    -s:b compiler.libcxx=libstdc++11 \
     -b
